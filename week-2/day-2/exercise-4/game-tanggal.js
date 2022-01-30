591129dd0ee9468ee40ac3e74b595d09933311a8
ahyaralfaruq <ahyaralfaruq97@gmail.com>
2022-01-31 04:33:33 +0700

    latihan day 2 exercise 4 membuat  tanggal

---
+++ week-2/day-2/exercise-4/game-tanggal.js
@@ -0,0 +1,57 @@
+let tanggal = 1
+let bulan = 11;
+let tahun = 1945
+
+switch (bulan) {
+    case 1:
+        bulan = `januari`
+        console.log(`${tanggal} ${bulan} ${tahun}`)
+        break;
+    case 2:
+        bulan = `februari`
+        console.log(`${tanggal} ${bulan} ${tahun}`)
+        break;
+    case 3:
+        bulan = `maret`
+        console.log(`${tanggal} ${bulan} ${tahun}`)
+        break;
+    case 4:
+        bulan = `april`
+        console.log(`${tanggal} ${bulan} ${tahun}`)
+        break;
+    case 5:
+        bulan = `mei`
+        console.log(`${tanggal} ${bulan} ${tahun}`)
+        break;
+    case 6:
+        bulan = `juni`
+        console.log(`${tanggal} ${bulan} ${tahun}`)
+        break;
+    case 7:
+        bulan = `juli`
+        console.log(`${tanggal} ${bulan} ${tahun}`)
+        break;
+    case 8:
+        bulan = `agustus`
+        console.log(`${tanggal} ${bulan} ${tahun}`)
+        break;
+    case 9:
+        bulan = `september`
+        console.log(`${tanggal} ${bulan} ${tahun}`)
+        break;
+    case 10:
+        bulan = `oktober`
+        console.log(`${tanggal} ${bulan} ${tahun}`)
+        break;
+    case 11:
+        bulan = `november`
+        console.log(`${tanggal} ${bulan} ${tahun}`)
+        break;
+    case 12:
+        bulan = `desember`
+        console.log(`${tanggal} ${bulan} ${tahun}`)
+        break;
+
+    default:
+        break;
+}
